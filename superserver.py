6b00eadea73044f0e3c3c89cf2427c731a3cd9c4
Daniel Farkas <w.farkasdaniel@gmail.com>
2017-08-18 14:49:53 +0200

    csv insert

--- superserver.py
+++ superserver.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, redirect, request, session
+import csv
 
 app = Flask(__name__)
 
+
+def csv_reader(csv):
+    with open(csv, mode='r') as csv_file:
+        text = csv.reader(csv_file)
+        text_list = list(text)
+        return text_list
+
 @app.route('/')
 def route_list():
-
-    return render_template('list.html')
+    text_list = csv_reader("data.csv")
+    return render_template('list.html', text_list=text_list)
 
 
 @app.route('/story')
