17a583df3da49ad230b786eec0ef80d20d6a1279
Daniel Farkas <w.farkasdaniel@gmail.com>
2017-08-19 20:14:26 +0200

    fixing bugs, delete is still missing

--- superserver.py
+++ superserver.py
@@ -12,31 +12,25 @@ def csv_reader(file_name):
 
 
 @app.route('/')
+@app.route('/list')
 def route_index():
     text_list = csv_reader("data.csv")
     return render_template('list.html', text_list=text_list)
 
 
-@app.route('/list')
-def route_list():
-    return route_index()
-
-
 @app.route('/story')
 def route_story():
-    edit = False
     text_list = csv_reader("data.csv")
+    user_id = 0
     for i in range(len(text_list)):
         if text_list[i][0]:
             user_id = int(text_list[i][0]) + 1
-        else:
-            user_id = 0
+    print(user_id)
     return render_template('form.html', user_id=user_id)
 
 
 @app.route('/save-story', methods=['POST'])
 def route_save():
-    print('POST request received!')
     if request.method == 'POST':
         title = request.form['title']
         story = request.form['story']
@@ -54,22 +48,18 @@ def route_save():
                 'status': status})
     return redirect('/')
 
+
 @app.route('/story/<id>')
 def route_story_id(id=None):
-    edit = True
     user_id = id
-    id = int(id)
+    edit = True
     text_list = csv_reader("data.csv")
-    new_text_list = text_list[id]
-    fieldnames = ['user_id', 'title', 'story', 'criteria', 'value', 'estimation', 'status']
-    return render_template('form.html', user_id=user_id, edit=edit, fieldnames=fieldnames, new_text_list=new_text_list)
+    new_text_list = text_list[int(id)]
+    return render_template('form.html', user_id=user_id, edit=edit, new_text_list=new_text_list)
 
 @app.route('/edit-story/<id>', methods=['POST'])
 def route_edit_story(id=None):
     text_list = csv_reader("data.csv")
-    user_id = id
-    id = int(id)
-    print('POST EDIT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! request received!')
     if request.method == 'POST':
         title = request.form['title']
         story = request.form['story']
@@ -78,13 +68,11 @@ def route_edit_story(id=None):
         estimation = request.form['estimation']
         status = request.form['status']
         user_id = request.form.get("user_id")
-        fieldnames = ['user_id', 'title', 'story', 'criteria', 'value', 'estimation', 'status']
-        with open("data.csv", "w", newline="") as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-            writer.writerow({
-                'user_id': user_id, 'title': title, 'story': story,
-                'criteria': criteria, 'value': value, 'estimation': estimation,
-                'status': status})
+        user_id = int(user_id)
+        text_list[user_id] = [user_id, title, story, criteria, value, estimation, status]
+        with open("data.csv", "w", newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(text_list)
     return redirect('/')
 
 
